d3a5c5655ff0d9e2ae0e8e87094705bb343536b4
Jerry Hodges <[jhodge10]@users.noreply.github.com>
2026-06-20 17:59:16 -0600

    json and server updates

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const express = require('express');
+import express from 'express';
+import session from 'express-session';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.get('/', (req, res) => {
-  res.send('Amiibo Library API is running!');
+  res.render('home');
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
