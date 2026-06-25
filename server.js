d7d84657892d8d864793723abf6b5fa17d37ad42
Jerry Hodges <[jhodge10]@users.noreply.github.com>
2026-06-25 15:12:51 -0600

    foundation created for the website. Able to display intro on local host

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import session from 'express-session';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,20 +10,12 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  })
-);
-
 app.get('/', (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
